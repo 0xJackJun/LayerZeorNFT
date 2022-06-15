78e4b9a43d4cfec6ce86e29b5579d758712badbf
quanrong <jackqr1996@gmail.com>
2022-06-15 20:49:12 +0800

    add multilple chain id

---
+++ args/arbitrumRinkeby.js
@@ -0,0 +1,6 @@
+module.exports = [
+    "https://ipfs.io/ipfs/QmYD1cZ47HFcEXi5jU9tRDYZuirEpow16sqrPVN88gQAKR/",
+    "0x4D747149A57923Beb89f22E6B7B97f7D8c087A00",
+    1,
+    2222
+];
---
+++ args/fuji.js
@@ -0,0 +1,6 @@
+module.exports = [
+    "https://ipfs.io/ipfs/QmYD1cZ47HFcEXi5jU9tRDYZuirEpow16sqrPVN88gQAKR/",
+    "0x93f54D755A063cE7bB9e6Ac47Eccc8e33411d706",
+    1,
+    2222
+];
